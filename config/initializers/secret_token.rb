a2f02c61a010b611661e55d417c039eaa7382390
Tahira <tahira.adaya@gmail.com>
2014-04-03 15:58:10 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hangerz::Application.config.secret_key_base = 'a6a9debc5a0d2b93b6efcc45a3479ac3e31747437d1e37ddb0ca665e7619338200be77faa39d6cc370a07fe8b5c77bf61e61957c653bca24bbaee2e8fc0afd9f'
